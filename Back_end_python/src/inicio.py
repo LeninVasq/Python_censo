37790f111b43a78c71abda7309cb0558d0f8ba77
LeninVasq <150496561+LeninVasq@users.noreply.github.com>
2024-12-16 21:28:19 -0600

    Discapacidades

--- Back_end_python/src/inicio.py
+++ Back_end_python/src/inicio.py
@@ -1,9 +1,56 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from app import get_data_poblacion
-
+from discapacidades import get_data_discapacidades
+from discapacidades1 import get_data_discapacidades1
+from poblacion import get_data_poblacion
+from idiomas import get_data_idiomas
+from esctructuras import get_data_estructuras
+from etnia import get_data_etnia
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/api/etnia", methods=['GET'])
+def get_etnia():
+    data = get_data_etnia()
+    
+    return jsonify({
+        "data": data,
+        
+        
+})
+
+
+@app.route("/api/esctructuras", methods=['GET'])
+def get_esctructuras():
+    data = get_data_estructuras()
+    
+    return jsonify({
+        "data": data,
+        
+        
+})
+
+@app.route("/api/discapacidades", methods=['GET'])
+def get_discapacidades():
+    data = get_data_discapacidades()
+    
+    return jsonify({
+        "data": data,
+        
+        
+})
+    
+@app.route("/api/discapacidades1", methods=['GET'])
+def get_discapacidades1():
+    data = get_data_discapacidades1()
+    
+    return jsonify({
+        "data": data,
+        
+        
+})    
+    
+
 @app.route("/api/poblacion", methods=['GET'])
 def get_poblacion():
     data = get_data_poblacion()
@@ -12,8 +59,18 @@ def get_poblacion():
         "data": data,
         
         
-    })
+})
 
+
+@app.route("/api/idiomas", methods=['GET'])
+def get_idiomas():
+    data = get_data_idiomas()
+    
+    return jsonify({
+        "data": data,
+        
+        
+})
 
 
 
--- Back_end_python/src/poblacion.py
+++ Back_end_python/src/poblacion.py
@@ -1,27 +1,34 @@
 import openpyxl
 
 def get_data_poblacion(file_path="../db/poblacion.xlsx"):
-    # Cargar el archivo de Excel
+
     excel_dataframe = openpyxl.load_workbook(file_path)
     dataframe = excel_dataframe.active
 
     data = []
     
     distribucion_total = 0.0
+    veces = 0
+    
 
     suma_columna_3 = sum(
         row[2].value for row in dataframe.iter_rows(min_row=2, max_row=dataframe.max_row) 
         if isinstance(row[2].value, (int, float)) 
     )
-    # Iterar sobre las filas y recopilar datos
+
     for row in dataframe.iter_rows(min_row=2, max_row=dataframe.max_row, max_col=dataframe.max_column):
         _row = [cell.value for cell in row] 
         
-        if isinstance(row[2].value, (int, float)) and suma_columna_3 != 0:
+        if(veces == 0 ):
+            fila_4 = "Distribucion Total"
+            veces += 1
+        
+        elif isinstance(row[2].value, (int, float)) and suma_columna_3 != 0:
             fila_4 = row[2].value / suma_columna_3
             distribucion_total +=  row[2].value / suma_columna_3
         else:
             fila_4 = None  
+
         
         _row.append(fila_4) 
         del _row[4]
